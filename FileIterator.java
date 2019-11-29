79c7a30d65da3e84a0e7f85eb4efcaecd7856653
rogovrt <49844050+rogovrt@users.noreply.github.com>
2019-11-29 14:13:05 +0300

    Add files via upload

---
+++ FileIterator.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.NoSuchElementException;
+
+public class FileIterator {
+    private BufferedReader buf;
+    private static final int MAX_LEN = 1;
+
+    public FileIterator(String path) {
+        try {
+            FileReader fr = new FileReader(path);
+            buf = new BufferedReader(fr);
+        }
+        catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public boolean hasNext() {
+        try {
+            buf.mark(MAX_LEN);
+            String s = buf.readLine();
+            buf.reset();
+            return (s != null);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public String next() {
+        if (this.hasNext()) {
+            try {
+                return buf.readLine();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+        throw new NoSuchElementException();
+    }
+}
